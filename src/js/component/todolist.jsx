7a0d65a3e3bdfc65fc7498717873373edfde020e
santiagoe16 <santiagoespinal333@gmail.com>
2024-09-10 04:17:28 +0000

    Todolist with memory

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,11 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-
-//		insertar el valor al array
-//		mapeo el array y lo devuelvo en varios li y un ul
-// 		añado los botones de eliminar y su funcionalidad
-
-//create your first component
 const Todolist = () => {
 
 	const [list, setList] = useState([]);
@@ -13,29 +7,59 @@ const Todolist = () => {
 	const [itemLeft, setItemLeft] = useState(0)
 	const [hoveredIndex, setHoveredIndex] = useState(null);
 
-	const handleKey = (e) =>{
+	let newArrassy = [];
+
+	useEffect(()=>{
+		fetch('https://playground.4geeks.com/todo/users/santiagoe16')
+		.then(resp => {
+			return resp.json();
+		})
+		.then(data => {
+		setList(data.todos.map((item)=>item));
+		})
+	},[])
+	
+
+	const addTask = (e) =>{
 		if(e.key === "Enter"){
 			if(inputValue.trim() !== ""){
-				setList([...list, inputValue]);
-				setInputValue("")
 				setItemLeft(list.length + 1)
+				
+				const requestOptions = {
+					method: 'POST',
+					headers: { 'Content-Type': 'application/json' },
+					body: JSON.stringify({
+						"label": inputValue,
+						"is_done": false
+					})
+				};
+				fetch('https://playground.4geeks.com/todo/todos/santiagoe16', requestOptions)
+				.then(response => response.json())
+				.then(data => {
+					setList([...list, data]);
+				})
+
+				setInputValue("")
 			}
 		}
 	}
 
-	const deleteTask = (index) => {
+	const deleteTask = (element,index) => {
 		setList(list.filter((_,i) => i !== index))
 		setItemLeft(list.length - 1)
-		
+
+		fetch(`https://playground.4geeks.com/todo/todos/${element.id}`, {method: 'DELETE'})
+		.then((response) => response.text())
+		.then((result) => console.log(result))
 	}
 	
 	return (
 		<>
 			<h1 className="d-flex justify-content-center">todos</h1>
 			<div className="m-auto" id="list">
-				<input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKey} placeholder="What needs to be done?"/>
+				<input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={addTask} placeholder="What needs to be done?"/>
 				<ul>
-					{list.map((element, index) => (<li onMouseOver={()=>setHoveredIndex(index)} onMouseOut={()=>setHoveredIndex(index)} key={index}>{element} <i style={{visibility: hoveredIndex === index ? 'visible' : 'hidden'}} className="fas fa-times mt-1 ms-2 me-2" id="icon" onClick={() => deleteTask(index)}></i></li>))}
+					{list.map((element, index) => (<li onMouseOver={()=>setHoveredIndex(index)} onMouseOut={()=>setHoveredIndex(index)} key={index}>{element.label}<i style={{visibility: hoveredIndex === index ? 'visible' : 'hidden'}} className="fas fa-times mt-1 ms-2 me-2" onClick={() => deleteTask(element,index)}></i></li>))}
 				</ul>
 				<footer>{itemLeft} item left</footer>
 			</div>
